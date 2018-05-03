b418ac18f0315043bab4bf70db58d26262be0692
Nino <nino.vitale@icloud.com>
2018-05-03 16:14:05 +0100

    Added test for discord vote regex.

--- HackQTests/DiscordTriviaTest.swift
+++ HackQTests/DiscordTriviaTest.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2018 Gordon Jacobs. All rights reserved.
 //
 
-import Foundation
+import XCTest
+
+@testable import HackQ
+
+class DiscordTriviaTest: XCTestCase {
+    private let discordTrivia = DiscordTrivia(triviaShow: .hq)
+    
+    func testisValidVote() {
+        let votes = ["1", "2", "3", "4", "1 apg", "1?????", "21", "10.5", "its 3"]
+        let expectations = [true, true, true, false, true, true, false, false, true]
+        
+        let isValid = votes.map { discordTrivia.isValidVote($0) }
+        
+        XCTAssertEqual(isValid, expectations)
+    }
+}
