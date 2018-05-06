77d88c8b7b97e387d7ce112bed31a89effe2d462
Nino <nino.vitale@icloud.com>
2018-05-06 22:25:30 +0100

    App will quit after closing last window.

--- HackQ/AppDelegate.swift
+++ HackQ/AppDelegate.swift
@@ -70,13 +70,7 @@ struct Config {
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
     }
 }
